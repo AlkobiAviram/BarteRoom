2405985393483631abc9e6f1e7774fbf5d747093
gili123 <gili1054@gmail.com>
2015-05-17 19:04:22 +0300

    DB

---
+++ NewProject/BarteRoom/BarteRoom/DB.cs
@@ -0,0 +1,79 @@
+﻿/*
+ *BarterRoom - DataBase handler class  
+ * Date: 17/05/2015
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Data;
+
+namespace BarteRoom
+{
+    public class DB
+    {
+        private SqlConnection connect;
+        private SqlCommand command;
+        private SqlDataReader rdr;
+        private string query;
+
+
+        //constructor - initialize the connection to the Database
+        public DB()
+        {
+            connect = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        }
+
+
+        /*
+         * adding new user to the Database
+         * if the adding fails from some resion return false
+         * if the adding success return true
+        */
+        public bool addNewUser(String usr, String password, String fullName, String email, int manager)
+        {
+            query = "insert into Users values('" + usr + "','" + password + "','" + fullName + "','" + email + "'," + manager + ");"; 
+
+            try
+            {
+                connect.Open();
+
+                command = new SqlCommand(query, connect);
+
+                command.ExecuteNonQuery();
+                connect.Close();
+            }
+
+            catch (Exception e) { return false; }
+
+            return true;
+        }
+
+        
+        //return the full name of a specific user 
+        public String getFullName(String usr)
+        {
+            string Name = "";
+            query = "select fullName from users where usr = '" + usr + "';";
+
+            try
+            {
+                connect.Open();
+               
+                command = new SqlCommand(query, connect);
+
+                Name = command.ExecuteScalar().ToString();
+                connect.Close();
+            }
+
+            catch (Exception e) { }
+
+            return Name;
+        }
+    }
+}
