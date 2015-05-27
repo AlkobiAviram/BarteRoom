e1bd49461bceb54be683891f91f9c109dfab8ec5
gabaynati <gabaynati@gmail.com>
2015-05-27 13:51:21 +0200

    class constructor

--- BarteRoom/Item.cs
+++ BarteRoom/Item.cs
@@ -7,13 +7,22 @@ namespace BarteRoom
 {
     public class Item
     {
+        private String usr;
         private String name;
-        private Byte[] pic_name;
+        private String clss;
         private String comments;
         private String description;
-        private String usr;
-        private String clss;
+        private Byte[] pic_name;
 
+        public Item(String usr, String name, String clss, String comments, String description, Byte[] pic_name)
+        {
+            this.usr = usr;
+            this.name = name;
+            this.clss = clss;
+            this.comments = comments;
+            this.description = description;
+            this.pic_name = pic_name;
+        }
 
     }
 }
