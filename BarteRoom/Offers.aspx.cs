2067e2811a6e3442d23ed020d184a9f451d9a27b
Aviram alkobi <aviramalkobi@gmail.com>
2015-06-05 03:25:22 +0300

    undo offers

--- BarteRoom/Offers.aspx.cs
+++ BarteRoom/Offers.aspx.cs
@@ -7,47 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace BarteRoom
 {
-    public partial class BidsOffersInbox : System.Web.UI.Page
+    public partial class Offers : System.Web.UI.Page
     {
-        Logic lg = new Logic();
-
         protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-
-                bind();
-            }
-        }
-        private void bind()
-        {
-
-            GridView1.DataSource = lg.getDataSourceForBids(Session["usr"].ToString());
-            GridView1.DataBind();
-
-        }
-
-        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
-        {
-
-            if (e.CommandName == "Select")
-            {
-            }
-        }
-
-
-        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
-        {
-            int index = Convert.ToInt32(e.NewSelectedIndex);
-            GridViewRow row = GridView1.Rows[index];
-            Session["bid_id"] = row.Cells[1].Text;
-            Response.Redirect("/TransactionView.aspx");
-            GridView1.EditIndex = -1;
-            bind();
-
-        }
-
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
